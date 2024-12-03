d99d9c512df6bba4b4e11243ab0a1c82efa8f6e0
bideex24 <bideex24@gmail.com>
2024-12-04 00:42:33 +0600

    production

--- src/components/Homepage/SDLC.jsx
+++ src/components/Homepage/SDLC.jsx
@@ -1,140 +1,182 @@
-import React from 'react';
-import SectionTitle from '../shared/SectionTitle';
+import React from "react";
+import SectionTitle from "../shared/SectionTitle";
 
 const SDLC = () => {
-    return (
-        <div>
-            <SectionTitle heading="SDLC"></SectionTitle>
-            <div className="p-4 max-w-[1440px] mx-auto">
-                <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-                    <li>
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-start mb-10 md:text-end">
-                            {/* <time className="font-mono italic">1984</time> */}
-                            <div className="text-lg font-black">Planning</div>
-                            Every successful work starts with proper planning. A luminous plan can act as the core phase of the software development process. Finding out the new scopes, resource availability, Timeframe, costing more over the dream for a successful business depends on a perfect plan. We always keep us ready to make a perfect plan for you.
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end mb-10">
-                            {/* <time className="font-mono italic">1998</time> */}
-                            <div className="text-lg font-black">Analysis</div>
-                            It helps to sketch the far-flung potential of development strategy. A critical analysis opens the door to a successful project. What to design and how to develop it, in brief, it’s the stage to justify the idea of the project. Adjusting to the client’s need and designing the project to the end is the main objective of Analysis.
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-start mb-10 md:text-end">
-                            {/* <time className="font-mono italic">2001</time> */}
-                            <div className="text-lg font-black">Design</div>
-                            Designing is one of the most important stages of the Software development cycle. However, a true design never falls you down from your success. All Front-End and Back-End designs are brought into the mind through the experiences of developers in this stage. A precise design makes sure the proper functionality and development of the process.
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end mb-10">
-                            {/* <time className="font-mono italic">2007</time> */}
-                            <div className="text-lg font-black">Development & Testing</div>
-                            The development process includes coding and developing the documentation. It’s the mother part of a project where the entire documents are assembled, and the functionalities are developed. In a nutshell, this stage brings life to software. After developing till the end, the process is taken into Test to check the bug or error.
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-start mb-10 md:text-end">
-                            {/* <time className="font-mono italic">2001</time> */}
-                            <div className="text-lg font-black">Implementation</div>
-                            After passing the testing process successfully, the software is ready for implantation. It's the final stage or market-ready condition of the project. In this stage, the product is openly launched and gets customer feedback. If it requires any change depending on customer feedback or client's satisfaction, developers fix them.
-                        </div>
-                        <hr />
-                    </li>
-                    <li>
-                        <hr />
-                        <div className="timeline-middle">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                                className="h-5 w-5">
-                                <path
-                                    fillRule="evenodd"
-                                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                    clipRule="evenodd" />
-                            </svg>
-                        </div>
-                        <div className="timeline-end mb-10">
-                            {/* <time className="font-mono italic">2007</time> */}
-                            <div className="text-lg font-black">Maintenance</div>
-                            Maintenance is the operational control of the system after launching the project in the market. When the software is developed unerringly and implemented successfully it goes under maintenance by the developers for keeping it up to date with timeframe, fixing bug issues, and overall ensuring that the system is running smoothly.
-                        </div>
-                    </li>
-                </ul>
+  return (
+    <div>
+      <SectionTitle heading="SDLC"></SectionTitle>
+      <div className="p-4 max-w-[1440px] mx-auto">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </div>
-        </div>
-    );
+            <div className="timeline-start mb-10 md:text-end">
+              {/* <time className="font-mono italic">1984</time> */}
+              <div className="text-lg font-black">Planning</div>
+              Every successful work starts with proper planning. A luminous plan
+              can act as the core phase of the software development process.
+              Finding out the new scopes, resource availability, Timeframe,
+              costing more over the dream for a successful business depends on a
+              perfect plan. We always keep us ready to make a perfect plan for
+              you.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end mb-10">
+              {/* <time className="font-mono italic">1998</time> */}
+              <div className="text-lg font-black">Analysis</div>
+              It helps to sketch the far-flung potential of development
+              strategy. A critical analysis opens the door to a successful
+              project. What to design and how to develop it, in brief, it’s the
+              stage to justify the idea of the project. Adjusting to the
+              client’s need and designing the project to the end is the main
+              objective of Analysis.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              {/* <time className="font-mono italic">2001</time> */}
+              <div className="text-lg font-black">Design</div>
+              Designing is one of the most important stages of the Software
+              development cycle. However, a true design never falls you down
+              from your success. All Front-End and Back-End designs are brought
+              into the mind through the experiences of developers in this stage.
+              A precise design makes sure the proper functionality and
+              development of the process.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end mb-10">
+              {/* <time className="font-mono italic">2007</time> */}
+              <div className="text-lg font-black">Development & Testing</div>
+              The development process includes coding and developing the
+              documentation. It’s the mother part of a project where the entire
+              documents are assembled, and the functionalities are developed. In
+              a nutshell, this stage brings life to software. After developing
+              till the end, the process is taken into Test to check the bug or
+              error.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              {/* <time className="font-mono italic">2001</time> */}
+              <div className="text-lg font-black">Implementation</div>
+              After passing the testing process successfully, the software is
+              ready for implantation. Its the final stage or market-ready
+              condition of the project. In this stage, the product is openly
+              launched and gets customer feedback. If it requires any change
+              depending on customer feedback or clients satisfaction, developers
+              fix them.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end mb-10">
+              {/* <time className="font-mono italic">2007</time> */}
+              <div className="text-lg font-black">Maintenance</div>
+              Maintenance is the operational control of the system after
+              launching the project in the market. When the software is
+              developed unerringly and implemented successfully it goes under
+              maintenance by the developers for keeping it up to date with
+              timeframe, fixing bug issues, and overall ensuring that the system
+              is running smoothly.
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SDLC;
